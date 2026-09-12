4102ee2e782cd7b76b8d30b35182dd56b1f5064e
Carl <113813911+cgutierrez234@users.noreply.github.com>
2026-09-11 22:17:57 -0600

    Added functionality to load jobs from JSON config

--- main.py
+++ main.py
@@ -2,21 +2,23 @@ from job import Job
 from executor import Executor
 from job_queue import JobQueue
 
+import json
+
 if __name__ == "__main__":
 
-    job = Job("succeeding-job", "true")
-    job2 = Job("failing-job", "false")
-    job3 = Job("really-failing-job", "this_is_bogus")
-    job4 = Job("timeout_job", "sleep 10", timeout=2)
 
     job_queue = JobQueue()
 
-    job_queue.add_job(job)
-    job_queue.add_job(job2)
-    job_queue.add_job(job3)
-    job_queue.add_job(job4)
-    
+    with open("jobs.json", "r") as file:
+       jobs =  json.load(file)
 
+       for job in jobs:
+           name = job["job_name"]
+           command = job["job_command"]
+           timeout = job["timeout"]
+           new_job = Job(name, command, timeout)
+           job_queue.add_job(new_job)
+    
     my_exec = Executor()
 
     while job_queue.has_jobs():
