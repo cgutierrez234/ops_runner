cc021b4d1c9ac9a601658354087f47c1b774eb57
Carl <113813911+cgutierrez234@users.noreply.github.com>
2026-09-12 22:54:07 -0600

    Added loading and validation of jobs from JSON config

--- main.py
+++ main.py
@@ -3,21 +3,46 @@ from executor import Executor
 from job_queue import JobQueue
 
 import json
+import sys
 
 if __name__ == "__main__":
 
 
     job_queue = JobQueue()
 
-    with open("jobs.json", "r") as file:
-       jobs =  json.load(file)
+    try:
 
-       for job in jobs:
-           name = job["job_name"]
-           command = job["job_command"]
-           timeout = job["timeout"]
-           new_job = Job(name, command, timeout)
-           job_queue.add_job(new_job)
+        with open("jobs.json", "r") as file:
+            jobs =  json.load(file)
+            
+    except FileNotFoundError:
+        print("jobs.json not found")
+        sys.exit(1) # <--- end the python program right now.
+
+    except json.JSONDecodeError:
+        print("jobs.json config file is malformed.")
+        sys.exit(1)
+
+    for job in jobs:
+        try:
+            name = job["job_name"]
+            if not isinstance(name, str):
+                print(f"Job '{name}' has an invalid name: {name!r}")
+                continue
+            command = job["job_command"]
+            if not isinstance(command, str):
+                print(f"Job '{name}' has an invalid command: {command!r}")
+                continue
+            timeout = job.get("timeout")
+            if not isinstance(timeout, int) and timeout is not None:
+                print(f"Job '{name}' has an invalid timeout: {timeout!r}")
+                continue
+        except KeyError as error:
+                print(error)
+                continue
+        
+        new_job = Job(name, command, timeout)
+        job_queue.add_job(new_job)
     
     my_exec = Executor()
 
