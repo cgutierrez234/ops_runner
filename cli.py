66f687e757424eaad8ab0c7175427a8d1a51f16a
Carl <113813911+cgutierrez234@users.noreply.github.com>
2026-09-17 14:53:27 -0500

    Added interactive job selection and application loop

--- cli.py
+++ cli.py
@@ -1,4 +1,30 @@
 from cleanup_config import CleanupConfig
+from job import Job
+
+import os
+
+def display_main_menu() -> int:
+    menu_options = ["Run Jobs", "Run Cleanup", "Exit"]
+    
+    for index, option in enumerate(menu_options):
+        print(f"{index + 1}. {option}")
+    
+    while True:
+
+        startup_choice = input("Please make your selection: ")
+
+        try:
+            startup_choice = int(startup_choice)
+            if startup_choice < 1 or startup_choice > len(menu_options):
+                print(f"Your selection needs to to be from the list of options")
+                continue
+            else:
+                break
+        except ValueError:
+            continue
+
+    return startup_choice
+
 
 def select_cleanup_config(cleanup_configs: list[CleanupConfig]) ->CleanupConfig:
 
@@ -18,9 +44,38 @@ def select_cleanup_config(cleanup_configs: list[CleanupConfig]) ->CleanupConfig:
             else:
                 break
         except ValueError:
-            # print(f"{user_choice} is an invalid type. Please re-enter.")
             continue
 
     user_choice = user_choice - 1
 
     return cleanup_configs[user_choice]
+
+def select_job(jobs: list[Job]) -> Job:
+
+    for index, job in enumerate(jobs):
+        print(f"{index + 1}. {job}")
+
+    while True: 
+        user_choice = input("Please select the Job you want to perform(a numeric option you've been provided)")
+
+        try:
+            user_choice = int(user_choice)
+            if user_choice < 1 or user_choice > len(jobs):
+                print(f"Your selection needs to to be from the list of options")
+                continue
+            else:
+                break
+        except ValueError:
+            continue
+    user_choice = user_choice - 1
+
+    return jobs[user_choice]
+
+def clear_screen():
+    if os.name == "nt":
+        os.system("cls")
+    else:
+        os.system("clear")
+        print("\033[3J", end="")
+
+    
--- main.py
+++ main.py
@@ -3,36 +3,51 @@ from job_queue import JobQueue
 from pathlib import Path
 from cleanup import run_cleanup
 from loader import load_jobs, load_cleanup_configs
-from cli import select_cleanup_config
+from cli import  clear_screen, display_main_menu,  select_cleanup_config, select_job
 
 if __name__ == "__main__":
     # Set home directory
     home = Path.home()
 
-# Load the jobs from their JSON config. 
+# Load the jobs from their JSON config. Happens at intiation of ops-runner. Objects are waiting in memory
     jobs_json_to_load = home / "Desktop" / "ops_runner"/ "jobs.json"
     jobs = load_jobs(jobs_json_to_load)
 
-    # Load the cleanup_config from its JSON file. 
+    # Load the cleanup_config from its JSON file. Happens at intiation of ops-runner. Objects are waiting in memory
     cleanup_config_to_load = home / "Desktop" / "ops_runner" / "cleanup.json"
     cleanup_configs = load_cleanup_configs(cleanup_config_to_load)
 
-    job_queue = JobQueue()
+    while True:
 
-    for job in jobs:
+        startup_choice = display_main_menu()
+        match startup_choice:
+            case 1:
 
-        job_queue.add_job(job)
+                clear_screen()
+                selected_job = select_job(jobs)
+
+                job_queue = JobQueue()
+                job_queue.add_job(selected_job)
+                
+                my_exec = Executor()
+
+                while job_queue.has_jobs():
+                    job_to_do = job_queue.pop_job()
+                    my_result = my_exec.run_job(job_to_do)
+                    print(my_result)
+            case 2:
+                continue
+            case 3:
+                break
+            
+
     
-    my_exec = Executor()
-
-    while job_queue.has_jobs():
-        job_to_do = job_queue.pop_job()
-        my_result = my_exec.run_job(job_to_do)
-        print(my_result)
 
 
-config_to_run = select_cleanup_config(cleanup_configs)
-run_cleanup(config_to_run)
+
+
+# config_to_run = select_cleanup_config(cleanup_configs)
+# run_cleanup(config_to_run)
 
     
 
